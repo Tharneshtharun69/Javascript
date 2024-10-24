e406685ec1e0b15656d3dae54c4f0bc0a5b9e632
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2024-10-24 08:23:06 +0530

    Update

---
+++ object.js
@@ -0,0 +1,9 @@
+// JavaScript Objects
+const person={name:'Tharnesh',age:20};
+console.log(person.name);
+console.log(person.age);
+const person1={};
+person1.name="Arun";
+person1.age=20;
+console.log(person1.name);
+console.log(person1.age);
