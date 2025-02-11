fcc1d7a09f362697daf98acc0c8674bed124f383
Tharnesh R <125240513+Tharneshtharun69@users.noreply.github.com>
2025-02-11 18:25:03 +0530

    Create script.js

---
+++ DOM/script.js
@@ -0,0 +1,16 @@
+var a=document.getElementById("para");
+function show(){
+  a.textContent="Hello World";
+}
+function hide(){
+  a.textContent="";
+}
+function changebg(){
+  document.body.style.backgroundColor="red";
+}
+function changetext(){
+  a.textContent="Hello";
+}
+function reset(){
+  document.body.style.backgroundColor="white";
+}
